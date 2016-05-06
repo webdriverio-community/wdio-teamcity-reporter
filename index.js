27ad3a188cbe1157a1e1d4e019d075a4bb510e83
Alexey Litvinov <sullenor@yandex-team.ru>
2016-05-06 18:44:47 +0300

    eslint added

--- index.js
+++ index.js
@@ -82,14 +82,14 @@ function escape(str) {
 
   return str
     .toString()
-    .replace(/\x1B.*?m/g, "")
-    .replace(/\|/g, "||")
-    .replace(/\n/g, "|n")
-    .replace(/\r/g, "|r")
-    .replace(/\[/g, "|[")
-    .replace(/\]/g, "|]")
-    .replace(/\u0085/g, "|x")
-    .replace(/\u2028/g, "|l")
-    .replace(/\u2029/g, "|p")
-    .replace(/'/g, "|'");
+    .replace(/\x1B.*?m/g, '')
+    .replace(/\|/g, '||')
+    .replace(/\n/g, '|n')
+    .replace(/\r/g, '|r')
+    .replace(/\[/g, '|[')
+    .replace(/\]/g, '|]')
+    .replace(/\u0085/g, '|x')
+    .replace(/\u2028/g, '|l')
+    .replace(/\u2029/g, '|p')
+    .replace(/'/g, '|\'');
 }
