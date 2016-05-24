a4acef0e4860548eac2895870c806815d9eef62e
Alexey Litvinov <sullenor@yandex-team.ru>
2016-05-24 09:09:24 +0300

    escape policy update

---
+++ test/unit/escape.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const { escape } = require('../../lib/message');
+const test = require('tape');
+
+test('escape', t => {
+  t.equal(escape('comma \' in the text'), 'comma |\' in the text');
+  t.equal(escape('\u0085\u2028\u2029'), '|x|l|p');
+  t.end();
+});
