858fc60c88e712b12ed7de4592fa8a0451d56423
Victor <victor@turo.com>
2019-03-06 16:39:08 +0000

    feat(reporter): use new reporter API

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 'use strict';
 
-const { EventEmitter } = require('events');
+const WdioReporter = require('@wdio/reporter').default;
 const { buildFormatter, events } = require('./lib/message');
 const { flow } = require('lodash');
-const { inherits } = require('util');
-
-module.exports = WdioTeamcityReporter;
-module.exports.reporterName = 'teamcity';
 
 /**
  * @param {object}  baseReporter
@@ -17,36 +13,35 @@ module.exports.reporterName = 'teamcity';
  * @param {string}  reporterOptions.message
  * @return {wdioTeamcityReporter}
  */
-function WdioTeamcityReporter(baseReporter, wdioConf, reporterOptions = {}) {
-  this.baseReporter = baseReporter;
+class WdioTeamcityReporter extends WdioReporter {
+  constructor(reporterOptions) {
+    Object.assign(reporterOptions, {
+      captureStandardOutput: typeof reporterOptions.captureStandardOutput === 'boolean'
+        ? reporterOptions.captureStandardOutput
+        : false,
+      flowId: typeof reporterOptions.flowId === 'boolean'
+        ? reporterOptions.flowId
+        : true,
+      name: typeof reporterOptions.message === 'string'
+        ? reporterOptions.message
+        : '[title]',
+      stdout: true,
+    });
+    super(reporterOptions);
+    this.enableRealTimeOutput(reporterOptions);
+  }
 
-  const opts = {
-    captureStandardOutput: typeof reporterOptions.captureStandardOutput === 'boolean'
-      ? reporterOptions.captureStandardOutput
-      : false,
-    flowId: typeof reporterOptions.flowId === 'boolean'
-      ? reporterOptions.flowId
-      : true,
-    name: typeof reporterOptions.message === 'string'
-      ? reporterOptions.message
-      : '[title]',
-  };
-
-  this.enableRealTimeOutput(opts);
-}
-
-inherits(WdioTeamcityReporter, EventEmitter);
-
-WdioTeamcityReporter.prototype.enableRealTimeOutput = function (opts) {
-  events.forEach(event => this.on(event, flow(buildFormatter(event, opts), output)));
-};
-
-/**
- * @param  {string} msg
- * @return {string}
- */
-function output(msg) {
-  if (msg) {
-    console.log(msg);
+  enableRealTimeOutput(opts) {
+    events.forEach(event => {
+      this[event] = (...args) =>
+        flow(buildFormatter(event, opts), msg => {
+          if (msg) {
+            this.write(msg);
+          }
+        })(...args);
+    });
   }
 }
+
+module.exports = WdioTeamcityReporter;
+module.exports.reporterName = 'teamcity';
